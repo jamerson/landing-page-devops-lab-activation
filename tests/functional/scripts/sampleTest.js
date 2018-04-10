29d199b003978a50ccb46a1b94d8acd05cf9185c
jamerson <jamerson.lima@gmail.com>
2018-04-10 10:02:16 -0300

    fix

--- tests/functional/scripts/sampleTest.js
+++ tests/functional/scripts/sampleTest.js
@@ -2,13 +2,15 @@ module.exports = {
   'Sample functional test' : function (browser) {
     browser
       .url('http://lab-activation-happyday.mybluemix.net/')
-      .waitForElementVisible('#inputemail', 60000, true,
+      .waitForElementVisible('#gotocampaign', 60000, true,
         function(){}, 'Waiting for Go To Campaign Button')
-      .click()
-      .waitForElementPresent('#inputemail', 60000)
+      .click('#gotocampaign')
+      .waitForElementPresent('#inputemail', 60000, true,
+        function(){}, 'Waiting for Input Email input')
       .setValue('#inputemail', ['test@test.com', browser.Keys.ENTER])
-      .waitForElementPresent('#registeremail', 60000)
-      .click()
+      .waitForElementPresent('#registeremail', 60000, true,
+        function(){}, 'Waiting for Register EMail Button')
+      .click('#registeremail')
       .pause(5000)
       .end();
   }
